062d6bdd73fa3e00ecd6e12c6ef72bd3ea1d5233
dhiaa <jh_loukkaf@esi.dz>
2023-04-14 21:30:07 +0100

    Zeniq 2077

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Zeniq 2077" />
+        <meta name="author" content="ashendhiaa" />
+        <title>Zeniq 2077</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
